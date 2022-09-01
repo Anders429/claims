6b485ccd429fcb11697940ad373cfdd1c74c40e5
Matt Wilkinson <matt@mattwilkinson.dev>
2022-08-31 23:08:52 -0400

    0.7.1
    Signed-off-by: Matt Wilkinson <matt@mattwilkinson.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(rustc_1_6, no_std)]
-#![doc(html_root_url = "https://docs.rs/claims/0.7.0")]
+#![doc(html_root_url = "https://docs.rs/claims/0.7.1")]
 #![allow(unknown_lints, unused_extern_crates)]
 #![forbid(
     rust_2018_idioms,
